a1236f7f83bb55a54c6ab76ddf7de1c3250d0256
YaoSir <2332281629@qq.com>
2020-01-03 17:55:53 +0800

    ww

--- luckin.py
+++ luckin.py
@@ -50,49 +50,61 @@ def verification(X_DEVICE_ID = None, X_APP_ID = None):
         "current_cash": data["current_cash"],
         "current_token": data["current_token"]
     }
+    try:
+        HOSTNAME = "rm-uf6ktwa39f10394a7no.mysql.rds.aliyuncs.com"
+        PORT = "3306"
+        DATABASE = "pigfarmdb"
+        USERNAME = "myadmin"
+        PASSWORD = "GGhavefun123"
 
-    HOSTNAME = "rm-uf6ktwa39f10394a7no.mysql.rds.aliyuncs.com"
-    PORT = "3306"
-    DATABASE = "pigfarmdb"
-    USERNAME = "myadmin"
-    PASSWORD = "GGhavefun123"
+        DB_URI = "mysql+pymysql://{username}:{password}@{host}:{port}/{db}?charset=utf8".\
+        format(username=USERNAME,password=PASSWORD,host=HOSTNAME,port=PORT,db=DATABASE)
 
-    DB_URI = "mysql+pymysql://{username}:{password}@{host}:{port}/{db}?charset=utf8".\
-    format(username=USERNAME,password=PASSWORD,host=HOSTNAME,port=PORT,db=DATABASE)
+        engine = create_engine(DB_URI)
+        conn = engine.connect()
 
-    engine = create_engine(DB_URI)
-    conn = engine.connect()
+        # ground_truth_list = []
+        result = conn.execute("select username, current_cash, current_token from user where user_id={user_id};".format(user_id=params["user_id"]))
+        ground_truth_list = result.fetchall()
+        if ground_truth_list[0][0] != params["username"]:
+            error = json.dumps({"error" : "username mismatch!"})
+            return json_response(error, 403)
+        if ground_truth_list[0][1] != params["current_cash"]:
+            error = json.dumps({"error" : "current_cash mismatch!"})
+            return json_response(error, 403)
+        if ground_truth_list[0][2] != params["current_token"]:
+            error = json.dumps({"error" : "current_token mismatch!"})
+            return json_response(error, 403)
 
-    # ground_truth_list = []
-    result = conn.execute("select username, current_cash, current_token from user where user_id={user_id};".format(user_id=params["user_id"]))
-    ground_truth_list = result.fetchall()
-    if ground_truth_list[0][0] != params["username"]:
-        error = json.dumps({"error" : "username incorrect!"})
+        # then perform updates on last_login_date
+        result = conn.execute("select timestampdiff(second, user.last_login_date, CURRENT_TIMESTAMP) from user where user_id={user_id};".format(user_id=params["user_id"]))
+        timediff = result.fetchall()
+        timediff = timediff[0][0]
+        if timediff > 86400: # exceed one day
+            result = conn.execute("update user set last_login_date = CURRENT_TIMESTAMP where user_id={user_id};".format(user_id=params["user_id"]))
+        else: # doesn't exceed one day
+            pass
+        current_last_login_date = conn.execute("select last_login_date from user where user_id={user_id};".format(user_id=params["user_id"]))
+        current_last_login_date = current_last_login_date[0][0]
+        conn.close()
+        return json_response(json.dumps({"user_id" : params["user_id"], "last_login_date" : current_last_login_date}), 200)
+    except Exception as e:
+        conn.close()
+        error = json.dumps({"error" : e})
         return json_response(error, 403)
-    if ground_truth_list[0][1] != params["current_cash"]:
-        error = json.dumps({"error" : "current_cash incorrect!"})
-        return json_response(error, 403)
-    if ground_truth_list[0][2] != params["current_token"]:
-        error = json.dumps({"error" : "current_token incorrect!"})
-        return json_response(error, 403)
-
-    # then perform updates on last_login_date
-    result = conn.execute("select timestampdiff(second, user.last_login_date, CURRENT_TIMESTAMP) from user where user_id={user_id};".format(user_id=params["user_id"]))
-    last_login_date_list = result.fetchall()
-    
-
-
-    
-
-    conn.close()
-    raise NotImplementedError
 
 
 
 # 
 @app.route("/items/<int:id>")
 def items(id = None, X_APP_ID = None):
-    raise NotImplementedError
+    if id is None:
+        error = json.dumps({"error" : "Non existing id!"})
+        return json_response(error, 400)
+    if X_APP_ID is None:
+        error = json.dumps({"error" : "Missing X-APP-ID!"})
+        return json_response(error, 401)
+    
 
 
 
